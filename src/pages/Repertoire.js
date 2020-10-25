eb90ae31e77c3d7e87c4510ce32376698775889d
Selena Siri <selenasiri@gmail.com>
2020-10-25 12:16:14 -0400

    repertoire

--- src/pages/Repertoire.js
+++ src/pages/Repertoire.js
@@ -3,7 +3,91 @@ import React from 'react'
 const repertoire = () => {
   return (
     <div>
-      Repertoire
+      <p>
+            The following are a list of songs and arias from the standard classical repertoire 
+            that range from beginning to advanced difficulty levels. After working on these songs myself, 
+            I have found that maintaining the appropriate repertoire can help a singer 
+            keep their voice on track! Just as an athlete must continue to stretch and exercise 
+            on a regular basis, singers must treat their instruments in the same respect. Many pieces 
+            on this list can also be adjusted to suit the singer's natural 'tessitura', or in other words 
+            it is the pitch range where it is the most comfortable to sing. </p>
+            
+            
+            <h2>Opera</h2>
+            
+            
+
+            <p>Be good to her, be kind from The Saint of Bleeker Street by Gian Carlo Menotti
+            Deh vieni, non tardar from Le nozze di Figaro by Mozart
+            Frère voyez... Du Gai Soleil from Werther by Massenet
+            In uomini, in soldati from Così fan tutte by Mozart
+            Je veux vivre from Roméo et Juliette by Charles Gounod
+            L'ho perduta from Le nozze di Figaro by Mozart
+            My Darling Jim from Glory Denied by Tom Cipullo
+            O mio babbino caro from Gianni Schicchi by Puccini
+            S'altro che lacrime from La Clemenza di Tito by Mozart
+            The Black Swan from The Medium by Gian Carlo Menotti
+            The Willow Song from The Ballad of Baby Doe by Douglas Moore
+            Tornami a vagheggiar from Alcina by George Frideric Handel
+            The Silver Aria from The Ballad of Baby Doe by Douglas Moore
+            What Good Would The Moon Be? from Street Scene by Kurt Weill</p>
+            
+            <h2>Art Songs</h2>
+             
+            
+            <p>Alleluja by Mozart 
+            Caro mio ben by Giuseppe Giordani
+            Come Ready and See Me by Richard Hundley
+            Dans les ruines d'une abbaye by Fauré
+            E l'uccellino by Puccini
+            Frülingsglaube by Schubert
+            Già'il sole dal gange by Alessandro Scarlatti
+            I Attempt from Love's Sickness to Fly by Henry Purcell
+            I Love All Graceful Things by Eric Harding Thiman
+            Il bacio by Luigi Arditi
+            La primavera by Puccini
+            Le Secret by Fauré
+            Lerchengesang by Johannes Brahms
+            Nacht und Träume by Schubert
+            N'est-ce pas? by Fauré
+            Per pietà bell'idol mio by Vincenzo Bellini
+            Seligkeit by Franz Schubert
+            Stabat Mater by Giovanni Battista Pergolesi 
+            Terra e mare by Puccini
+            Une Sainte en son auréole by Fauré 
+            Voyage à Paris from Banalités by Poulenc
+            When I Have Sung My Songs by Ernest Charles
+            Widmung by Robert Schumann
+            Wie Melodien zieht es mir by Johannes Brahms
+            3 Shakespeare Songs Op. 37 no. 3 by Amy Beach:
+            O Mistress Mine
+            Take, O take those lips away
+            Fairy Lullaby</p>
+            
+            
+            
+            <h2>Musical Theatre</h2>
+            
+            
+            
+            <p>Bridges of Madison County: Almost real
+            Into the Woods: Cinderella, Rapunzel
+            Once on this Island: Ti Moune
+            West Side Story: I Feel Pretty, I Have a Love, One Hand, One Heart (duet), Tonight (duet)
+            110 in the Shade: Is it Really Me?</p>
+            
+            
+            <h1>Chinese Pop & Traditional</h1>
+            
+            <p>我爱你中国 "I love you, China"
+            改变自己"Change Myself"
+            你怎么舍得我难过 "How could you leave me in sorrow?"
+            原来你怎么都不想要 "So you actually don't want anything at all?"
+            你的歌声里 "You exist in my song"
+            茉莉花 "Jasmine Flower"
+            多谢了 "Many thanks" from The Three Liu Sisters 
+            唱山哥 "Sing a Mountain Song"
+          </p>
     </div>
   )
 }
