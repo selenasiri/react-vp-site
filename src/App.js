7caefe85b29b3ec6e622dc7cf12619fe63749cdf
Selena Siri <selenasiri@gmail.com>
2020-10-24 23:36:28 -0400

    basic navbar setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Navbar from './components/Navbar'
 import './App.css';
 
 //components
@@ -14,6 +15,7 @@ function App() {
     <Provider store={store}>
       <Router>
         <div>
+          <Navbar />
           <Switch>
             <Route exact path='/' component={Landing} />
             <Route exact path='/about' component={About} />
