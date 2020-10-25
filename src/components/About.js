0d6d07583cc8129ff634df0f2a67a82775ab5108
Selena Siri <selenasiri@gmail.com>
2020-10-24 23:13:32 -0400

    about

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,10 @@ import React from 'react'
 const about = () => {
   return (
     <div>
-      About Page
+      <h2>Bio</h2>
+      <p>I am very excited to teach musical genres including musical theater, classical, opera, R&B, pop and jazz for voice and piano. I am currently working with students from ages 4-65 and would encourage anyone who wants to explore their musical potential to take a chance putting themselves out there. I have witnessed music being an outlet for many people, offering relief from our mundane daily lives, and it has been known to release stress, improve memory, help with speech problems, teach empathy, remind us to breathe and strengthen our deepest intuitions. </p>
+
+      <p>Singing has become a way of living- a form of expression and I can't wait to continue helping my students find their own artistic outlets as well. I am also a member of the New York Singing Teachers' Association and the National Association of Teachers of Singing and teach lessons regarding vocal technique, repertoire coaching, dramatic or acting coaching, career counseling, music notation and theory. Please do not hesitate to reach out with any further questions!</p>
     </div>
   )
 }
