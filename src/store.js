5d7567ac595ab130247bec7de4f65edb537c20b3
Selena Siri <selenasiri@gmail.com>
2020-10-25 23:18:59 -0400

    beginning store setup

--- src/store.js
+++ src/store.js
@@ -1,8 +1,16 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { applyMiddleware, configureStore } from "@reduxjs/toolkit";
 import rootReducer from './reducers/rootReducer';
+import thunk from 'redux-thunk'
+import index from './reducers/index'
 
-// const initialState = {};
+const initialState = {};
 
-const store = configureStore({reducer:  rootReducer});
+const middleware = [thunk];
+
+const store = configureStore(
+  rootReducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
 
 export default store;
