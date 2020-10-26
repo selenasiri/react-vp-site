d1a14247c7a5b5418aec3f19f4fb1695677a3db7
Selena Siri <selenasiri@gmail.com>
2020-10-25 21:35:33 -0400

    basic navbar on top

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,32 @@
 import React, { Component } from "react";
+import { Link } from 'react-router-dom'
  
 class NavBar extends Component {
   render() {
     return (
       <React.Fragment>
-        <nav className="navbar navbar-dark bg-dark mb-3">
-          <a className="navbar-brand" href="#">
-            <h1>Total Items <span className="badge badge-secondary">{this.props.totalItems}</span></h1>
-          </a>
-        </nav>
+       <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <Link to="navbar-brand" href="#">Navbar w/ text</Link>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarText">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <Link to="/" >Home <span class="sr-only">(current)</span></Link>
+      </li>
+      <li class="nav-item">
+        <Link to="/about">About</Link>
+      </li>
+      <li class="nav-item">
+        <Link to="greatDivas">The Great Divas</Link>
+      </li>
+    </ul>
+    <span class="navbar-text">
+      Navbar text with an inline element
+    </span>
+  </div>
+</nav>
       </React.Fragment>
     );
   }
