554ab7427ee340b7dadeb0ec45eba8be65e1ab76
Selena Siri <selenasiri@gmail.com>
2020-10-28 20:58:31 -0400

    link tags

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,28 +4,28 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-    <a className="navbar-brand" href="#">Navbar</a>
-    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
+        <Link className="navbar-brand" href="#">Navbar</Link>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+         <span className="navbar-toggler-icon"></span>
     </button>
-    <div className="collapse navbar-collapse" id="navbarNav">
-      <ul className="navbar-nav">
-        <li className="nav-item active">
-          <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Features</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Pricing</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-        </li>
-      </ul>
-    </div>
-  </nav>
-  )
-}
+       <div className="collapse navbar-collapse" id="navbarNav">
+         <ul className="navbar-nav">
+           <li className="nav-item active">
+              <Link className="nav-link" href="#">Home <span className="sr-only">(current)</span></Link>
+           </li>
+            <li className="nav-item">
+             <Link className="nav-link" href="#">Features</Link>
+            </li>
+           <li className="nav-item">
+             <Link className="nav-link" href="#">Pricing</Link>
+            </li>
+           <li className="nav-item">
+             <Link className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      )
+    }
 
 export default Navbar
